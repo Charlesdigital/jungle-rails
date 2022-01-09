8eb08c042b8250c42398f6c2c8643a9722057c27
Charlesdigital <charles.easton123@gmail.com>
2022-01-09 03:02:27 -0500

    add about page

---
+++ app/views/about/about_controller.rb
@@ -0,0 +1,11 @@
+<section class="about-show">
+
+  <header class="page-header">
+    <h1>About Page</h1>
+  </header>
+
+  <div class="view">
+    <p> Random products you will enjoy!</p>
+  </div>
+
+</section>
