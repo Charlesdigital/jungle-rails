8169e8ac4cfe8b5ccf0973d9082628f7e6e2c1ab
Charlesdigital <charles.easton123@gmail.com>
2022-01-11 13:02:52 -0500

    add test to make sure user fields are filled and should have a first name

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,28 +1,28 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe User, type: :model do
-#     describe 'Validations' do
-#       # Initial test
-#     #   it 'should create a new user when all fields are filled' do
-#     #     @user = User.new(
-#     #       first_name: 'First',
-#     #       last_name: 'Last',
-#     #       email: 'test@email.com',
-#     #       password: 'supercoolpassword',
-#     #       password_confirmation: 'supercoolpassword'
-#     #     )
-#     #     expect(@user).to be_valid
-#     #     end
-#     #1. Validates that user has a first name
-#     it 'should not be valid without a first name' do
-#         @user = User.new(
-#           first_name: nil,
-#           last_name: 'Last',
-#           email: 'test@email.com',
-#           password: 'supercoolpassword',
-#           password_confirmation: 'supercoolpassword'
-#         )
-#         expect(@user).to_not be_valid
-#       end
-#     end
-# end
+RSpec.describe User, type: :model do
+    describe 'Validations' do
+    #   Initial test
+      it 'should create a new user when all fields are filled' do
+        @user = User.new(
+          first_name: 'First',
+          last_name: 'Last',
+          email: 'test@email.com',
+          password: '123',
+          password_confirmation: '123'
+        )
+        expect(@user).to be_valid
+        end
+    #1. Validates that user has a first name
+    it 'should not be valid without a first name' do
+        @user = User.new(
+          first_name: nil,
+          last_name: 'Last',
+          email: 'test@email.com',
+          password: '123',
+          password_confirmation: '123'
+        )
+        expect(@user).to_not be_valid
+      end
+    end
+end
