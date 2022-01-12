a9edcaa57de27139da7dc459c677f4fe3ce62407
Charlesdigital <charles.easton123@gmail.com>
2022-01-12 00:12:31 -0500

    add test for matching password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,22 +7,28 @@ RSpec.describe User, type: :model do
         @user = User.new(
           first_name: 'First',
           last_name: 'Last',
-          email: 'test@email.com',
+          email: 'test@gmail.com',
           password: '123',
           password_confirmation: '123'
         )
         expect(@user).to be_valid
         end
-    #1. Validates that user has a first name
-    it 'should not be valid without a first name' do
-        @user = User.new(
-          first_name: nil,
-          last_name: 'Last',
-          email: 'test@email.com',
-          password: '123',
-          password_confirmation: '123'
-        )
-        expect(@user).to_not be_valid
-      end
+
+   it "Don't save user when passwords don't match" do
+    user = User.new({
+      first_name: 'bob',
+      last_name: 'lee',
+      email: 'test@gmail.com',
+      password: '123',
+      password_confirmation: '123'
+    })
+    user.save
+
+    find_user = User.where(first_name: 'Melissa')
+    puts "Find user: #{find_user.inspect}"
+    expect(find_user).not_to exist
+  end
+
+
     end
 end
