b5d10f47a8afcf99c8af7093348477f62841c0b2
Charlesdigital <charles.easton123@gmail.com>
2022-01-12 00:23:50 -0500

    add user tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,8 @@
-require 'rails_helper'
-
 RSpec.describe User, type: :model do
+    # pending "add some examples to (or delete) #{__FILE__}"
     describe 'Validations' do
-    #   Initial test
-      it 'should create a new user when all fields are filled' do
+      # Initial test to ensure that when all fields are present, user is created
+      it 'should create a new user when all fields are added' do
         @user = User.new(
           first_name: 'First',
           last_name: 'Last',
@@ -12,37 +11,79 @@ RSpec.describe User, type: :model do
           password_confirmation: '123'
         )
         expect(@user).to be_valid
-        end
+      end
 
-   it "Don't save user when passwords don't match" do
-    user = User.new({
-      first_name: 'Bob',
-      last_name: 'Lee',
-      email: 'test@gmail.com',
-      password: '123',
-      password_confirmation: '123'
-    })
-    user.save
+      #1. Validates that user has a first name
+      it 'should not be valid without a first name' do
+        @user = User.new(
+          first_name: nil,
+          last_name: 'Last',
+          email: 'test@email.com',
+          password: '123',
+          password_confirmation: '123'
+        )
+        expect(@user).to_not be_valid
+      end
 
-    find_user = User.where(first_name: 'Melissa')
-    puts "Find user: #{find_user.inspect}"
-    expect(find_user).not_to exist
-  end
+      #2. Validates that user has a last name
+      it 'should not be valid without a last name' do
+        @user = User.new(
+          first_name: 'First',
+          last_name: nil,
+          email: 'test@email.com',
+          password: '123',
+          password_confirmation: '123'
+        )
+        expect(@user).to_not be_valid
+      end
 
-  it "Don't save a user if their email is not unique" do
-    user = User.new({
-      first_name: 'Bob',
-      last_name: 'Lee',
-      email: 'test@gmail.com',
-      password: '123',
-      password_confirmation: '123'
-    })
-    user.save
+      #3. Validates that user has an email
+      it 'should not be valid without an email' do
+        @user = User.new(
+          first_name: 'First',
+          last_name: 'Last',
+          email: nil,
+          password: '123',
+          password_confirmation: '123'
+        )
+        expect(@user).to_not be_valid
+      end
 
-    find_user = User.where(first_name: 'Mary')
-    puts "Find user: #{find_user.inspect}"
-    expect(find_user).not_to exist
-  end
+      #4. Validates that user has a password
+      it 'should not be valid without a password' do
+        @user = User.new(
+          first_name: 'First',
+          last_name: 'Last',
+          email: 'test@email.com',
+          password: nil,
+          password_confirmation: '123'
+        )
+        expect(@user).to_not be_valid
+      end
+
+      #5. Validates that user has added password confirmation
+      it 'should not be valid without a password confirmation' do
+        @user = User.new(
+          first_name: 'First',
+          last_name: 'Last',
+          email: 'test@email.com',
+          password: '123',
+          password_confirmation: nil
+        )
+        expect(@user).to_not be_valid
+      end
+
+      #6. It should throw an error when password confirmation does not match
+      it 'should not be valid if password is different' do
+        @user = User.new(
+          first_name: 'First',
+          last_name: 'Last',
+          email: 'test@email.com',
+          password: '123',
+          password_confirmation: '1234'
+        )
+        expect(@user).to_not be_valid
+      end
 
     end
-end
+  end
